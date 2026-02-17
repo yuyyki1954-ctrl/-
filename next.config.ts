664ad5b7fece8212a1552ee7f7dc72f5c4505483
Your Name <you@example.com>
2026-02-17 22:35:41 +0900

    Fix build: Remove invalid eslint config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
   },
